0fe741283ceb260d76b1eaace7846aed901c81de
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-08 11:30:30 -0700

    alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(ord('a'), ord('b')):
+for i in range(ord('a'), ord('z')):
     if chr(i) != 'e' and chr(i) != 'q':
-        print('{}'.format(i), end='')
+        print('{:c}'.format(i), end='')
