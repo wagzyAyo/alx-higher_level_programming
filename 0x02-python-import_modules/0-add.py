bbb6a3d959d58565b325da625aad12d8c001c891
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-10 11:06:51 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/pytthon3
 from add_0 import add
-add(a) = int(1)
-add(b) = int(2)
-result = a + b
-print({}"+" {} "=" {}.format(add(a),add(b),result), end="")
+a = 1
+b = 2
+print({:d}"+" {:d} "=" {:d}.format(a, b, add(a, b)))
