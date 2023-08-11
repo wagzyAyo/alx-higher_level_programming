e59597846be2113d3108623a7031d708f9501b63
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-11 09:09:35 -0700

    infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for add in range( len(sys.argv) - 1):
+    for add in range(len(sys.argv) - 1):
         total += int(sys.argv[add + 1])
-        print("{}".format(total))
+    print("{}".format(total))
