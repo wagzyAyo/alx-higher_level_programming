cccb8cf6cc40d419409e4056c08e91d5fa47883a
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-12 04:19:00 -0700

    calcilator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} = {}".format(a, sys.argv[2], b, opeartion[sys.argv[2]](a, b)))
+    print("{} {} {}  = {}".format(a, sys.argv[2], b, opeartion[sys.argv[2]](a, b)))
