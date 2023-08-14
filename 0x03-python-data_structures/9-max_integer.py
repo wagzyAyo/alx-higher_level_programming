b472f36f8121d2083259f15cc1f436adfbc6e7df
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-14 03:20:08 -0700

    highest integer

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    else:
+        highest_int = my_list[0]
+        for x in range(len( my_list)):
+            if my_list[x] > highest_int
+            highest_int = my_list[x]
+        return highest_int
