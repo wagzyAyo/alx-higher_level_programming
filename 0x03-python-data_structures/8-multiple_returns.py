affe8470ec42e4fc9b9f571c53ca39dcd9bd2628
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-14 03:10:31 -0700

    returns

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    if sentence != "":
+        first_char = setence[0]
+    else:
+        first_char = None
+
+    return (len(sentence), first_char)
