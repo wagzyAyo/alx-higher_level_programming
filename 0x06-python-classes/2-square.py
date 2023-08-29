d35f05d3f7fa906719d38ad3efbd2ac8a6d6d0a8
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-29 08:21:47 -0700

    2-square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''defines a square'''
+
+
+class Square:
+    '''Class Representing a square'''
+
+    def __init__(self, size=0):
+        '''size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
+         if size is less than 0, raise a ValueError exception with the message size must be >= 0'''
+
+         if not isinstance(size, int):
+             raise TypeError("Size must be an integer")
+         if size < 0:
+             raise ValueError("size must be >= 0")
+         self__size = size
