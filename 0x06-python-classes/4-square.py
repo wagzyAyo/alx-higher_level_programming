000f144c840f849f045f5a998e2639895cf8c08a
wagzyAyo <talktojmcvibes@gmail.com>
2023-08-29 09:28:41 -0700

    4-square

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,14 +11,15 @@ class Square:
         be an integer if size is less than 0, raise a
         ValueError exception with the message size
         must be >= 0'''
-        self.size = size
 
-        @property
-        def size(self):
-            return self.__size
 
-        @size.setter
-        if not isinstance(size, int):
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
