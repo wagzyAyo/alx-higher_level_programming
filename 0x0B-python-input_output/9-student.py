2c34556446067cde99c949012790756e6f3bad23
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-12 08:27:49 -0700

    9-student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module defines a class student"""
+
+
+class Student:
+    """A class student"""
+     def __init__(self, first_name, last_name, age):
+         """initializing a new student """
+         self.first_name = first_name
+         self.last_name = last_name
+         self.age = age
+
+    def to_json(self):
+        """Gets dictionary representation of student"""
+        return self.__dict__
