872308e3404c311ce8bd475cd2f5888df3af9d34
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-12 08:00:06 -0700

    5-main

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains a function
+that write to a file using json"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function writes to a file
+    using json"""
+    with open(filename, mode="w") as FILE:
+        json.dumps(my_obj, FILE)
