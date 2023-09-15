b050d47a16ba218aa98805c8598d192868f283bf
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-15 09:26:54 -0700

    setter added

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -31,3 +31,42 @@ class Rectangle(Base):
         def y(self):
             """get the value for y"""
             self.__y
+
+        @width.setter
+        def width(self, value):
+            """set the value of width"""
+            if (type(value) is not int):
+                raise TypeError("width must be an integer")
+            if (value <= 0):
+                raise ValueError("width must be > 0")
+            
+            self.__width = value
+
+
+        @height.setter
+        def height(self, value):
+            """set the value of height"""
+            if (type(value) is not int):
+                raise TypeError("height must be an integer")
+            if (value <=0):
+                raise ValueError("height must be > 0")
+
+            self.__height = value
+
+
+        @x.setter
+        def x(self, value):
+            """set the value for x"""
+            if (value < 0):
+                raise ValueError("x must be >= 0")
+
+            self.__x = value
+
+
+        @y.setter
+        def y(self, value):
+            """set the value of y"""
+            if (value < 0):
+                raise ValueError("y must be >= 0")
+
+            self.__y = value
