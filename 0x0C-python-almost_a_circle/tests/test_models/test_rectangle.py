18faa0242fe9fa0a44f796c0a7e40eab371540de
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-16 04:12:54 -0700

    rectangle test

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Test for rectangle class"""
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class test_rectangle(unittest.TestCase):
+    """test case for Rectangle class"""
+    def test_width(self):
+        """test width"""
+        self.assertEqual(7, self.r.width)
+
+    def tets_height(self):
+        """test height"""
+        self.assertEqual(10, self.r.width)
+
+    def test_width_neg(self):
+        """test width with negative number"""
+        with self.assertRaises(ValueError)
+        rect = Rectangle(-5, 2)
+
+    def test_height_neg(self):
+        """test height with negative number"""
+        with self.assertRaises(ValueError)
+        rect = Rectangle(5, -2)
+
+    def test_id(self):
+        """test rectangle id"""
+        rect = Rectangle(1,2,0,0,5)
+        self.assertEqual(5, rect.id)
