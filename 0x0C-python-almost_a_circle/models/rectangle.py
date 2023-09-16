d69c1233e63fe8daee6ecf44360fc018d1443eb3
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-16 07:36:06 -0700

    __str__ debug

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,5 +85,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """overriding string method"""
-        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} -
-        {self.__width/self.__height}")
+        return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+        {self.__width}/{self.__height}")
