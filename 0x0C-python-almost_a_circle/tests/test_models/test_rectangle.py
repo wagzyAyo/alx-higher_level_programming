698e76e9aefa3e87f2bc1ae3b9aa85454b7ce54d
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-17 02:40:00 -0700

    more rectangle test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -11,34 +11,68 @@ from models.rectangle import Rectangle
 from models.base import Base
 
 
-class test_rectangle(unittest.TestCase):
-    """test case for Rectangle class"""
+class TestRectangle(unittest.TestCase):
+    """Test case for Rectangle class"""
+
     def setUp(self):
         '''
-            Initializing instance with width and height
-            parameters
+        Initializing instance with width and height parameters
         '''
         self.r = Rectangle(5, 10)
-        
-    def test_width(self):
-        """test width"""
-        self.assertEqual(7, self.r.width)
 
-    def tets_height(self):
-        """test height"""
-        self.assertEqual(10, self.r.width)
+    def test_width(self):
+        """Test width"""
+        self.assertEqual(5, self.r.width)
+
+    def test_height(self):
+        """Test height"""
+        self.assertEqual(10, self.r.height)
 
     def test_width_neg(self):
-        """test width with negative number"""
-        with self.assertRaises(ValueError)
-        rect = Rectangle(-5, 2)
+        """Test width with negative number"""
+        with self.assertRaises(ValueError):
+            rect = Rectangle(-5, 2)
 
     def test_height_neg(self):
-        """test height with negative number"""
-        with self.assertRaises(ValueError)
-        rect = Rectangle(5, -2)
+        """Test height with negative number"""
+        with self.assertRaises(ValueError):
+            rect = Rectangle(5, -2)
 
     def test_id(self):
-        """test rectangle id"""
-        rect = Rectangle(1,2,0,0,5)
+        """Test rectangle id"""
+        rect = Rectangle(1, 2, 0, 0, 5)
         self.assertEqual(5, rect.id)
+
+    def test_area(self):
+    """Test the area() method"""
+    r = Rectangle(4, 5)
+    self.assertEqual(r.area(), 20)
+
+def test_display(self):
+    """Test the display() method"""
+    r = Rectangle(3, 2)
+    expected_output = "###\n###\n"
+
+    with StringIO() as captured_output:
+        r.display()
+        self.assertEqual(captured_output.getvalue(), expected_output)
+
+def test_str(self):
+    """Test the __str__() method"""
+    r = Rectangle(3, 4, 1, 2, 7)
+    self.assertEqual(str(r), "[Rectangle] (7) 1/2 - 3/4")
+
+def test_update(self):
+    """Test the update() method"""
+    r = Rectangle(2, 3, 4, 5, 6)
+    r.update(7)
+    self.assertEqual(str(r), "[Rectangle] (7) 4/5 - 2/3")
+    r.update(7, 8)
+    self.assertEqual(str(r), "[Rectangle] (7) 4/5 - 8/3")
+    r.update(7, 8, 9)
+    self.assertEqual(str(r), "[Rectangle] (7) 9/5 - 8/9")
+    r.update(7, 8, 9, 10)
+    self.assertEqual(str(r), "[Rectangle] (7) 9/10 - 8/9")
+    r.update(7, 8, 9, 10, 11)
+    self.assertEqual(str(r), "[Rectangle] (7) 9/10 - 8/9")
+
