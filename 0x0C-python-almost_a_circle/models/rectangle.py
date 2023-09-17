7a776d66ee34c5dd314f29244ec2200ef303d816
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-17 05:19:24 -0700

    debug

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
         return (f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
         {self.__width}/{self.__height}")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """This method assigns an
         argument to each attribute"""
         if args and len(args) != 0:
