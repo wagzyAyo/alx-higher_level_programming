1c1ca39cfab9ebffa55b17d02240dc847ec042c5
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-17 05:57:37 -0700

    arg and kwargs for square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,7 +30,41 @@ class Square(Rectangle):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-        self.__height = value
+        self.__height = valiue
+
+    def update(self, *args, **kwargs):
+        """This method updates attribute
+        of an instance"""
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if (type(args) != int and args[0] is not None:
+                        raise TypeError("id must be an integer")
+                self.id = args[0]
+
+            if len(args) > 1:
+                self.size = args[1]
+
+            if len(args) > 2:
+                self.x = args[2]
+
+            if len(args) > 3:
+                self.y = args[3]
+
+        else:
+        for key, value in kwargs.items():
+            if key == 'id':
+                if type(value) != int and value is not None:
+                    raise ValueError("id must be an integer")
+                self.id = value
+
+            if key == "size":
+                self.size = value
+            if key == 'x':
+                self.x = value
+            if key == 'y':
+                self.y = value
+
 
 
     def to_dictionary(self):
