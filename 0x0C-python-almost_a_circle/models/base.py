29da42388a428b1fa969ccb2733b85dee9dccfd0
wagzyAyo <talktojmcvibes@gmail.com>
2023-09-18 06:00:45 -0700

    from json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -25,6 +25,7 @@ class Base:
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """this method writes to a file"""
         file_name = cls.__name__ + ".json"
         with open(file_name, mode="w") as data_file:
             if list_objs is None:
@@ -32,3 +33,15 @@ class Base:
             else:
                 new_list = [item.to_dictionary() for item in list_objs]
                 data_file.write(Base.to_json_string(new_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """This method return list of json represented
+        by json_string"""
+        string_list = []
+
+        if json_string is None or json_string == '':
+            return string_list
+        else:
+            string_list = json.loads(json_string)
+            return string_list
