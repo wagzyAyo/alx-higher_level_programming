e95ecedd4adabd33014ba04a6434ba0d17695efa
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-13 07:35:42 -0800

    multi_c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar = 'C is fun'
+if (!isNaN(parseInt(process.argv[2]))) {
+	for (let x = 0; x < process.argv[2]; x++) {
+		console.log(myVar);
+	}
+} else {
+		console.log('Missing number of occurrences');
+	}
