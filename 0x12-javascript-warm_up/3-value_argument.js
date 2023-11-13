4823ff1c9d2c9f78bc7e029b654598edb3011011
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-13 06:27:52 -0800

    process.argv

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const argc = process.argv;
-
-if (argc[2] === undefined) {
+if (process.argv[2] === undefined) {
   console.log('No arguments');
 } else {
-  console.log(argc[2]);
+  console.log(process.argv[2]);
 }
