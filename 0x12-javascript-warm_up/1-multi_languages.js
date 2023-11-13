ac64b75c4354ea05cf5443c8093bda89391bf0f9
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-13 02:55:33 -0800

    C is fun, Python is cool, javascript is amazing

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+let myC = "C is fun";
+const myPython = "Python is cool";
+const myJavascript = "JavaScript is amazing";
+
+console.log(myC);
+console.log(myPython);
+console.log(myJavascript);
