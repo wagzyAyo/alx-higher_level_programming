b9264ee346c1f49c01fb6398926011f1980dccd3
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-13 07:39:58 -0800

    semistandard

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const myVar = 'C is fun'
+const myVar = 'C is fun';
 if (!isNaN(parseInt(process.argv[2]))) {
-	for (let x = 0; x < process.argv[2]; x++) {
-		console.log(myVar);
-	}
+  for (let x = 0; x < process.argv[2]; x++) {
+    console.log(myVar);
+  }
 } else {
-		console.log('Missing number of occurrences');
-	}
+  console.log('Missing number of occurrences');
+}
