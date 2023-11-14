d1c26454b621720a14f17e235476dcfc4a8e1ef0
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-14 07:29:04 -0800

    occurence

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+	let number = 0;
+	for (let x = 0; x < list.lenght; x++) {
+		if (searchElement === list[x] {
+			number++;
+		}
+	}
+	return number;
+}
