fa99fa44ca2d7bab4e633ffb8e625fe19fb53140
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-18 05:49:20 -0800

    model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+This module contain City class that inherits
+from Base imported from model_state
+"""
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
+
+
+class City(Base):
+    """
+    City class with id, name and state_id attributes
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False,
+                      ForeignKey=State.id)
