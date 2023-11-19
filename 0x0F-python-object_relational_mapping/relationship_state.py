fcd76c22d03a6d20c7e7a6654e15b7e0bd39e8f1
wagzyAyo <talktojmcvibes@gmail.com>
2023-11-18 22:18:04 -0800

    relationship

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the class definition of a State
+and an instance Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class with the attributes of id and name
+    for every instance of state"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True)
+    name = Column(String(128), nullable=False,)
+    ciities = relationship('City', backref='state', cascade='all, delete')
