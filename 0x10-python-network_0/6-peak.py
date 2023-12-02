94a0949d9e9aff3f451258c390f56b28fde43359
Ayo <106088806+wagzyAyo@users.noreply.github.com>
2023-12-02 18:01:50 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+'''Algorith that find the peak
+in a list of integers'''
+
+def find_peak(list_of_integers):
+    '''funtion returns peak in a list of integers'''
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # The peak must be on the left side, including mid
+            high = mid
+        else:
+            # The peak must be on the right side, excluding mid
+            low = mid + 1
+
+    return list_of_integers[low]
