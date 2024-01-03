dcd36596fec2d312de22cb8a10098a2ec55d112d
Ayo <talktojmcvibes@gmail.com>
2024-01-03 04:08:56 -0800

    write file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], utf-8, function (err, text){
+fs.readFile(process.argv[2], 'utf-8', function (err, text){
     if (text === undefined){
         console.log(err);
     } else{
