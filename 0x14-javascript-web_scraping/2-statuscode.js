3dcadc8ea59f3ac092715981060f12f0eedebb58
Ayo <talktojmcvibes@gmail.com>
2024-01-03 05:14:42 -0800

    4-count

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function(err, res){
-    if (err === null){
-        console.log('code: ' + res.statusCode);
-    }
+request(process.argv[2], function (err, res) {
+  if (err === null) {
+    console.log('code: ' + res.statusCode);
+  }
 });
