47c1a6f2a51cc4d13a3c6c9eb2f755a6a0982870
Ayo <talktojmcvibes@gmail.com>
2024-01-03 04:18:53 -0800

    2-status-code

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf-8', function (err, text){
-    if (text === undefined){
-        console.log(err);
-    } else{
-        console.log(text)
-    }
+fs.readFile(process.argv[2], 'utf-8', function (err, text) {
+  if (text === undefined) {
+    console.log(err);
+  } else {
+    console.log(text);
+  }
 });
